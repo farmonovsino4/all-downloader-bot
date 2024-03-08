7510aac407e3771b61e0978bbac70c0b943fe4f0
farmonovsino4 <farmonovsino3@gmail.com>
2024-03-08 12:35:15 +0500

    commit

--- downloader.py
+++ downloader.py
@@ -1,13 +1,16 @@
 import requests
+from environs import Env
 
+env = Env()
+env.read_env()
 
 def instagram_downloader(url):
     urll = "https://instagram-post-and-reels-downloader.p.rapidapi.com/"
 
-    querystring = {"url":url}
+    querystring = {"url": url}
 
     headers = {
-        "X-RapidAPI-Key": "3ced9d8030msh7126f2d249ae58ep13c95ajsn5107fb57f8a5",
+        "X-RapidAPI-Key": env.str('RAPIDAPI_KEY'),
         "X-RapidAPI-Host": "instagram-post-and-reels-downloader.p.rapidapi.com"
     }
 
