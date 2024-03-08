c7ff67e528c0e21c3cb32f628600e1a27333dd52
farmonovsino4 <farmonovsino3@gmail.com>
2024-03-08 18:26:27 +0500

    commit

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ async def echo(message: types.Message):
             response = requests.get(f"https://u11843.xvest5.ru/api/instagram/v1?url={message.text}")
             await message.answer_video(video=response.json()['medias'][0]['url'])
         except Exception as e:
-            await message.answer('Xatolik yuz berdi. Iltimos qayta urunib ko\'ring')
+            await message.answer('Videoni yuklayolmadim')
             await bot.send_message(chat_id=5230484991, text=e)
     elif message.text.startswith('https://www.tiktok.com/') or message.text.startswith('https://tiktok.com/') or message.text.startswith('https://vt.tiktok.com/') or message.text.startswith('https://www.vt.tiktok.com/'):
         try:
@@ -30,7 +30,7 @@ async def echo(message: types.Message):
             await message.answer("Yuklanmoqda")
             await message.answer_video(video=video['url'], caption=video['title'])
         except Exception as e:
-            await message.answer('Xatolik yuz berdi. Iltimos qayta urunib ko\'ring')            
+            await message.answer('Videoni yuklayolmadim')            
             await bot.send_message(chat_id=5230484991, text=e)
     elif message.text.startswith('https://www.facebook.com/') or message.text.startswith('https://facebook.com/') or message.text.startswith('https://fb.watch/') or message.text.startswith('https://www.fb.watch/'):
         try:
@@ -38,7 +38,7 @@ async def echo(message: types.Message):
             video = FacebookDownloader(message.text)
             await message.answer_video(video=video['url'], caption=video['title'])
         except Exception as e:
-            await message.answer('Xatolik yuz berdi. Iltimos qayta urunib ko\'ring')
+            await message.answer('Videoni yuklayolmadim')
             await bot.send_message(chat_id=5230484991, text=e)
     elif message.text.startswith('https://www.youtube.com/') or message.text.startswith('https://youtube.com/') or message.text.startswith('https://youtu.be/') or message.text.startswith('https://www.youtu.be/'):
         try:
@@ -48,6 +48,7 @@ async def echo(message: types.Message):
             with open(str(downloaded), 'rb') as video:
                 await message.answer_video(video=video, caption=yt.title)
         except Exception as e:
+            await message.answer('Videoni yuklayolmadim')
             await bot.send_message(chat_id=5230484991, text=e)
         try:
             os.remove(str(downloaded))
