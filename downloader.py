25caa2a3a6139e8c7e2bd3a8c5cbd9d844e00621
farmonovsino4 <farmonovsino3@gmail.com>
2024-03-08 17:37:41 +0500

    commit

--- downloader.py
+++ downloader.py
@@ -1,10 +1,12 @@
 import requests
 from environs import Env
+from pytube import YouTube
+
 
 env = Env()
 env.read_env()
 
-def tiktok_downloader(url):
+def TikTokdDownloader(url):
     urll = "https://tiktok-download-without-watermark.p.rapidapi.com/analysis"
 
     querystring = {"url": url,"hd":"0"} # https://vt.tiktok.com/ZSFy5K1Gr/
@@ -18,4 +20,18 @@ def tiktok_downloader(url):
 
     return {'url': response.json()['data']['play'], 'title': response.json()['data']['title']}
 
-print(tiktok_downloader("https://vt.tiktok.com/ZSFy5K1Gr/"))
+def FacebookDownloader(url):
+    urll = "https://facebook-reel-and-video-downloader.p.rapidapi.com/app/main.php"
+    # hard limit 500 request per month
+    querystring = {"url":url} # https://fb.watch/qGHemBLpH8/
+
+    headers = {
+    	"X-RapidAPI-Key": env.str('RAPIDAPI_KEY'),
+    	"X-RapidAPI-Host": "facebook-reel-and-video-downloader.p.rapidapi.com"
+    }
+
+    response = requests.get(urll, headers=headers, params=querystring)
+
+    return {'url': response.json()['links']['Download High Quality'], 'title': response.json()['title']}
+
+#'https://youtu.be/nT1dt6j4R8g?si=z7zDZ5PiiGQ406b0'
